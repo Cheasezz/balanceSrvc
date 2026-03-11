aea88a6776a4ed93b4013f00fd2358246a90ba9e
Iurii <yuradolgo@gmail.com>
2026-03-11 15:22:13 +0300

    init(server): Создал логгер slog

---
+++ cmd/balanceSrvc/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/Cheasezz/balanceSrvc/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Hiii its logger", slog.String("env", cfg.Env))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
